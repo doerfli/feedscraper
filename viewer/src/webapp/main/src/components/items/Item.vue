23560f12ac8d67fc411bd866575c5f2398523b0b
Marc Doerflinger <github@doerf.li>
2019-05-30 22:09:12 +0200

    show content on click

--- viewer/src/webapp/main/src/components/items/Item.vue
+++ viewer/src/webapp/main/src/components/items/Item.vue
@@ -1,14 +1,26 @@
 <template>
-  <li>
-    {{data.title}}
-  </li>
+    <li>
+        <span v-on:click="showContent">{{data.title}}</span>
+        <div v-if="isContentVisible" class="content" v-html="data.summary">
+        </div>
+    </li>
 </template>
 
 <script>
     export default {
         name: "Item",
         props: {
-            data: {}
+            data: {},
+        },
+        data: function () {
+            return {
+                isContentVisible: false
+            };
+        },
+        methods: {
+            showContent: function () {
+                this.isContentVisible = !this.isContentVisible
+            }
         }
     }
 </script>
