a97224b23487f19636bb3b01f1f1f1c5caf8285a
Marc Doerflinger <github@doerf.li>
2019-05-31 21:36:40 +0200

    add prefix api for controllers

--- viewer/src/webapp/main/src/http-common.js
+++ viewer/src/webapp/main/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const AXIOS = axios.create({
-    baseURL: `http://${process.env.VUE_APP_API_HOST}:8080`,
+    baseURL: `http://${process.env.VUE_APP_API_HOST}:8080/api`,
     headers: {
         // 'Access-Control-Allow-Origin': 'http://localhost:8070'
     }
