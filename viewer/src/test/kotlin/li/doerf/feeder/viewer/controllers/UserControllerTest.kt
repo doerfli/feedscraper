3782fa27d97597d3bb014217af2d664d6e87176c
Marc Doerflinger <github@doerf.li>
2019-06-03 00:06:35 +0200

    add tests

--- viewer/src/test/kotlin/li/doerf/feeder/viewer/controllers/UserControllerTest.kt
+++ viewer/src/test/kotlin/li/doerf/feeder/viewer/controllers/UserControllerTest.kt
@@ -1,5 +1,7 @@
 package li.doerf.feeder.viewer.controllers
 
+import li.doerf.feeder.common.entities.Role
+import li.doerf.feeder.common.entities.User
 import li.doerf.feeder.common.repositories.UserRepository
 import li.doerf.feeder.viewer.HttpException
 import org.assertj.core.api.Assertions.assertThatThrownBy
@@ -7,7 +9,9 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
@@ -21,9 +25,10 @@ class UserControllerTest {
 
     @Autowired
     private lateinit var mvc: MockMvc
-
     @Autowired
     private lateinit var userRepository: UserRepository
+    @Autowired
+    private lateinit var passwordEncoder: PasswordEncoder
 
     @Test
     fun testSignup() {
@@ -42,7 +47,6 @@ class UserControllerTest {
                 .andExpect(MockMvcResultMatchers.status().isOk)
                 .andExpect(MockMvcResultMatchers.jsonPath("$.token").isNotEmpty)
     }
-
 
     @Test
     fun testSignupAlreadyExists() {
@@ -71,5 +75,72 @@ class UserControllerTest {
                     // then
                     .andDo(MockMvcResultHandlers.print())
         }.hasCauseInstanceOf(HttpException::class.java)
+    }
+
+    @Test
+    fun testSignin() {
+        val user = User(0, "test@test123.com", passwordEncoder.encode("12345678"), mutableListOf(Role.ROLE_CLIENT))
+        userRepository.save(user)
+
+        // when
+        mvc.perform(MockMvcRequestBuilders.post("/api/users/signin")
+                .content("""
+                    {
+                        "username":"test@test123.com",
+                        "password": "12345678"
+                    }
+                """)
+                .contentType(MediaType.APPLICATION_JSON))
+
+                // then
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk)
+                .andExpect(MockMvcResultMatchers.jsonPath("$.token").isNotEmpty)
+    }
+
+    @Test
+    fun testSigninUsernameDoesNotExist() {
+        val user = User(0, "test@test123.com", passwordEncoder.encode("12345678"), mutableListOf(Role.ROLE_CLIENT))
+        userRepository.save(user)
+
+        // when
+        assertThatThrownBy {
+            mvc.perform(MockMvcRequestBuilders.post("/api/users/signin")
+                    .content("""
+                        {
+                            "username":"test1@test123.com",
+                            "password": "12345678"
+                        }
+                    """)
+                    .contentType(MediaType.APPLICATION_JSON))
+
+                    // then
+                    .andDo(MockMvcResultHandlers.print())
+                    .andExpect(MockMvcResultMatchers.status().isOk)
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.token").isNotEmpty)
+        }.hasCause(HttpException("Invalid username/password supplied", HttpStatus.INTERNAL_SERVER_ERROR))
+    }
+
+    @Test
+    fun testSigninInvalidPassword() {
+        val user = User(0, "test@test123.com", passwordEncoder.encode("12345678"), mutableListOf(Role.ROLE_CLIENT))
+        userRepository.save(user)
+
+        // when
+        assertThatThrownBy {
+            mvc.perform(MockMvcRequestBuilders.post("/api/users/signin")
+                    .content("""
+                        {
+                            "username":"test@test123.com",
+                            "password": "12345677"
+                        }
+                    """)
+                    .contentType(MediaType.APPLICATION_JSON))
+
+                    // then
+                    .andDo(MockMvcResultHandlers.print())
+                    .andExpect(MockMvcResultMatchers.status().isOk)
+                    .andExpect(MockMvcResultMatchers.jsonPath("$.token").isNotEmpty)
+        }.hasCause(HttpException("Invalid username/password supplied", HttpStatus.INTERNAL_SERVER_ERROR))
     }
 }
