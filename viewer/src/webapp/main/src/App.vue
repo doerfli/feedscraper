530faaa241e67d16a73927b0078b30b06b86df23
Marc Doerflinger <github@doerf.li>
2019-06-16 22:04:29 +0200

    switch to scss

--- viewer/src/webapp/main/src/App.vue
+++ viewer/src/webapp/main/src/App.vue
@@ -6,15 +6,18 @@
   </div>
 </template>
 
-<style lang="less">
-#app {
+<style lang="scss">
+  @import './../node_modules/bulma/bulma.sass';
+  @import './../node_modules/@fortawesome/fontawesome-free/css/all.css';
 
-}
+  #app {
 
+  }
 </style>
 <script>
     import Navigation from "./components/Navigation";
     import Messages from "./components/Messages";
+
     export default {
       components: {
         Navigation,
