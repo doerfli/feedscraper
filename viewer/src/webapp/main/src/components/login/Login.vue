8000b1b524c788da97cb23fff0d5eac7f43d3d85
Marc Doerflinger <github@doerf.li>
2019-06-22 21:55:28 +0200

    input validation for login form

--- viewer/src/webapp/main/src/components/login/Login.vue
+++ viewer/src/webapp/main/src/components/login/Login.vue
@@ -6,12 +6,19 @@
             </div>
             <div class="field-body">
                 <div class="field">
-                    <p class="control has-icons-left has-icons-right">
-                        <input class="input" type="email" placeholder="Email" v-model="username"/>
+                    <div class="control has-icons-left has-icons-right">
+                        <input
+                            v-model="username"
+                            v-on:change="validateUsername"
+                            v-bind:class="{input: true, 'is-success': this.validation.usernameValid}"
+                            type="email"
+                            placeholder="Email"
+                        />
                         <span class="icon is-small is-left">
                             <i class="fas fa-envelope"></i>
                         </span>
-                    </p>
+                    </div>
+                    <p v-if="!validation.usernamePattern" class="help is-danger">Please enter an email address</p>
                 </div>
             </div>
         </div>
@@ -21,12 +28,19 @@
             </div>
             <div class="field-body">
                 <div class="field">
-                    <p class="control has-icons-left has-icons-right">
-                        <input class="input" type="password" placeholder="Password" v-model="password"/>
+                    <div class="control has-icons-left has-icons-right">
+                        <input
+                            v-on:change="validatePassword"
+                            v-bind:class="{input: true, 'is-success': this.validation.passwordValid}"
+                            type="password"
+                            placeholder="Password"
+                            v-model="password"
+                        />
                         <span class="icon is-small is-left">
                             <i class="fas fa-lock"></i>
                         </span>
-                    </p>
+                    </div>
+                    <p v-if="!validation.passwordLength" class="help is-danger">Password must have at least 6 characters</p>
                 </div>
             </div>
         </div>
@@ -55,20 +69,32 @@
         data() {
             return {
                 username: "",
-                password: ""
+                password: "",
+                validation: {
+                    usernameValid: false,
+                    passwordValid: false,
+                    usernamePattern: true,
+                    passwordLength: true
+                }
             }
         },
         computed: {
             submitAllowed: function () {
-                let emailRex = /^(([^<>()\\[\]\\.,;:\s@"]+(\.[^<>()\\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-                let isAllowed = this.username.match(emailRex) != null;
-                isAllowed &= this.password.length >=6;
-                return isAllowed;
+                return this.validation.usernameValid && this.validation.passwordValid;
             }
         },
         methods: {
             login: function() {
                 this.$store.dispatch("users/login", {username: this.username, password: this.password})
+            },
+            validateUsername: function() {
+                let emailRex = /^(([^<>()\\[\]\\.,;:\s@"]+(\.[^<>()\\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                this.validation.usernamePattern = this.username.match(emailRex) != null;
+                this.validation.usernameValid = this.username.length > 0 && this.validation.usernamePattern;
+            },
+            validatePassword: function() {
+                this.validation.passwordLength = this.password.length >= 6;
+                this.validation.passwordValid = this.validation.passwordLength;
             }
         }
     };
