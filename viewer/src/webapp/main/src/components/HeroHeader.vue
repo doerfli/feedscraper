c37aafabc31338a3018aeac9084ebf9aff240cfc
Marc Doerflinger <github@doerf.li>
2019-11-10 13:46:00 +0100

    fix header style

--- viewer/src/webapp/main/src/components/HeroHeader.vue
+++ viewer/src/webapp/main/src/components/HeroHeader.vue
@@ -1,8 +1,8 @@
 <template>
-  <section>
+  <section class="container mx-auto mt-5 mb-6">
     <div>
-      <h1>{{title}}</h1>
-      <h2 v-if="subtitle">
+      <h1 class="antialiased font-bold capitalize tracking-wide text-3xl inline-block text-teal-700">{{title}}</h1>
+      <h2 class="antialiased font-medium text-1xl pl-3 inline-block" v-if="subtitle">
         {{subtitle}}
       </h2>
     </div>
